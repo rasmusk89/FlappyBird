4b7c107cddf00f71adf8553c82f76c7c7303b0d3
raskil <raskil@ttu.ee>
2021-02-21 18:46:13 +0200

    Another change

--- FlappyBird/src/index.js
+++ FlappyBird/src/index.js
@@ -63,7 +63,6 @@ function createColumn(colIndex) {
             }
         }
 
-
         rowElement.style.minHeight = rowHeight + 'px';
         rowElement.style.maxHeight = rowHeight + 'px';
         colElement.append(rowElement);
@@ -94,72 +93,3 @@ function moveScreen() {
 }
 
 moveScreen()
-
-
-/*
-for (let colIndex = 0; colIndex < COL_COUNT; colIndex++) {
-    let colElement = document.createElement('div');
-    colElement.classList.add('id-col-' + colIndex);
-    colElement.style.backgroundColor = '#FF00FF';
-    colElement.style.minWidth = colWidth + 'px';
-    colElement.style.maxWidth = colWidth + 'px';
-    colElement.style.float = 'left';
-    for (let rowIndex = 0; rowIndex < ROW_COUNT; rowIndex++) {
-        let rowElement = document.createElement('div');
-        rowElement.classList.add('id-col-' + colIndex + '-row-' + rowIndex);
-        if (colIndex % 2 == 0) {
-            if (rowIndex % 2 == 0) {
-                rowElement.style.backgroundColor = '#FF00FF'
-            } else {
-                rowElement.style.backgroundColor = '#00FFFF'
-            }
-        } else {
-            if (rowIndex % 2 == 0) {
-                rowElement.style.backgroundColor = '#00FFFF'
-            } else {
-                rowElement.style.backgroundColor = '#FF00FF'
-            }
-        }
-        rowElement.style.minHeight = rowHeight + 'px';
-        rowElement.style.maxHeight = rowHeight + 'px';
-        colElement.append(rowElement);
-
-    }
-    content.append(colElement)
-}
-*/
-
-
-/*
-for (let rowIndex = 0; rowIndex < ROW_COUNT; rowIndex++) {
-    let rowElement = document.createElement('div');
-    rowElement.style.minHeight = rowHeight + 'px';
-    rowElement.style.maxHeight = rowHeight + 'px';
-    rowElement.classList.add('id-row-' +rowIndex);
-    for (let colIndex = 0; colIndex < COL_COUNT; colIndex++) {
-
-        let colElement = document.createElement('div');
-        colElement.classList.add('id-row-' + rowIndex + '-col-' + colIndex)
-        if (rowIndex % 2 == 0) {
-            if(colIndex % 2 == 0) {
-                colElement.style.backgroundColor = '#FF00FF'
-            } else {
-                colElement.style.backgroundColor = '#00FFFF'
-            }
-        } else {
-            if(colIndex % 2 == 0) {
-                colElement.style.backgroundColor = '#00FFFF'
-            } else {
-                colElement.style.backgroundColor = '#FF00FF'
-            }
-        }
-        colElement.style.minWidth = colWidth + 'px';
-        colElement.style.minHeight = rowHeight + 'px';
-        colElement.style.display = 'inline-block';
-        rowElement.append(colElement)
-
-    }
-    content.append(rowElement)
-}
-*/
-
