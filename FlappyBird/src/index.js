9c9984d18286b8f7dc61e98f94defb3cf803bcd4
raskil <raskil@ttu.ee>
2021-02-23 23:08:42 +0200

    Created functions for creating columns

--- FlappyBird/src/index.js
+++ FlappyBird/src/index.js
@@ -1,5 +1,5 @@
 const ROW_COUNT = 40;
-const COL_COUNT = 50;
+const COL_COUNT = 60;
 
 let viewPortHeight = window.innerHeight;
 let rowHeight = viewPortHeight / ROW_COUNT;
@@ -9,87 +9,84 @@ let colWidth = viewPortWidth / COL_COUNT;
 
 document.body.style = 'margin: 0; padding: 0';
 
-let content = document.createElement('div');
+let gameBoard = document.createElement('div'); // game board Div
 
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromTo(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function createColumn(colIndex) {
-    let colElement = document.createElement('div');
+    let randomTop = randomIntFromTo(3, ROW_COUNT - 8);
+    let colElement = document.createElement('div'); // create column div
     colElement.classList.add('id-col-' + colIndex);
     colElement.style.minWidth = colWidth + 'px';
     colElement.style.maxWidth = colWidth + 'px';
     colElement.style.float = 'left';
 
-    let spaceBetween = 4;
-    let randomTop = randomIntFromInterval(3, ROW_COUNT - 7);
-
     for (let rowIndex = 0; rowIndex < ROW_COUNT; rowIndex++) {
-        let rowElement = document.createElement('div');
+        let rowElement = document.createElement('div'); // create row elements inside column div
         rowElement.classList.add('id-col-' + colIndex + '-row-' + rowIndex);
-
-        if (colIndex % Math.floor(COL_COUNT / 10 * 2) == 0) {
-            if (rowIndex < randomTop || rowIndex > randomTop + spaceBetween) {
-                if (colIndex % 2 == 0) {
-                    if (rowIndex % 2 == 0) {
-                        rowElement.style.backgroundColor = '#FF00FF'
-                    } else {
-                        rowElement.style.backgroundColor = '#00FFFF'
-                    }
-                } else {
-                    if (rowIndex % 2 == 0) {
-                        rowElement.style.backgroundColor = '#00FFFF'
-                    } else {
-                        rowElement.style.backgroundColor = '#FF00FF'
-                    }
-                }
-            }
-        } else {
-            if (rowIndex < 2 || rowIndex > ROW_COUNT - 3) {
-                if (colIndex % 2 == 0) {
-                    if (rowIndex % 2 == 0) {
-                        rowElement.style.backgroundColor = '#FF00FF'
-                    } else {
-                        rowElement.style.backgroundColor = '#00FFFF'
-                    }
-                } else {
-                    if (rowIndex % 2 == 0) {
-                        rowElement.style.backgroundColor = '#00FFFF'
-                    } else {
-                        rowElement.style.backgroundColor = '#FF00FF'
-                    }
-                }
-            }
-        }
-
         rowElement.style.minHeight = rowHeight + 'px';
         rowElement.style.maxHeight = rowHeight + 'px';
+        if (colIndex % Math.floor(COL_COUNT / 6 * 2) == 0) {
+            createObstacle(10, rowIndex, rowElement, randomTop);
+        } else {
+            createEdges(rowIndex, rowElement);        
+        }
         colElement.append(rowElement);
     }
     return colElement;
 }
 
-for (let colIndex = 0; colIndex < COL_COUNT; colIndex++) {
-    let colElement = createColumn(colIndex);
-    content.append(colElement);
 
+
+function createEdges(rowIndex, rowElement) {
+    if (rowIndex < 2 || rowIndex > ROW_COUNT - 3) { // create upper and lower edge
+        rowElement.style.backgroundColor = '#FF00FF';
+    }
 }
-document.body.append(content);
+
+function createObstacle(spaceBetween, rowIndex, rowElement, randomTop) {
+    if (rowIndex < randomTop || rowIndex > randomTop + spaceBetween) { // create obstacle
+        rowElement.style.backgroundColor = '#FF00FF'
+    }
+}
+
+function createMovingObstacle(spaceBetween, rowIndex, rowElement, randomTop) {
+    if (rowIndex < randomTop || rowIndex > randomTop + spaceBetween) { // create obstacle
+        rowElement.style.backgroundColor = '#FF00FF'
+        setInterval(() => {
+            
+        }, 100);
+    }
+}
+
+function createBoard() {
+    let board = document.createElement('div');
+    for (let colIndex = 0; colIndex < COL_COUNT; colIndex++) {
+        let colElement = createColumn(colIndex);
+        board.append(colElement);
+    }
+    return board;
+}
+
+gameBoard = createBoard()
+
+document.body.append(gameBoard);
 
 function moveScreen() {
 
     setInterval(() => {
-        let child = content.firstElementChild;
+        let child = gameBoard.firstElementChild;
         if (child !== null) {
             let childClassNameArray = child.className.split('-');
             let childId = childClassNameArray[childClassNameArray.length - 1]
             let colElement = createColumn(childId)
             child.remove();
-            content.append(colElement)
+            gameBoard.append(colElement)
         }
-    }, 100);
+    }, 1);
 
 }
 
-moveScreen()
+// moveScreen()
