78c3fca0cd88c1957a4054015b56caf6aa1eba55
raskil <raskil@ttu.ee>
2021-04-10 20:10:59 +0300

    Logout

--- kms-client/src/my-app.ts
+++ kms-client/src/my-app.ts
@@ -1,10 +1,20 @@
+import { IRouter } from "@aurelia/router";
 import { AppState } from "./state/app-state";
 
 export class MyApp {
-    constructor(private state: AppState) {
-        
+
+    constructor(
+        @IRouter private router: IRouter,
+        private state: AppState) {
     }
 
     attached() {
+    }
+
+    async logOut() {
+        this.state.token = null;
+        this.state.firstname = null;
+        this.state.lastname = null;
+        await this.router.load('home-index')
     }
 }
