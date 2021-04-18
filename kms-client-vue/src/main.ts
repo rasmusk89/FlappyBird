94286573e8846690af94ae17bd8152815b302aed
raskil <raskil@ttu.ee>
2021-04-18 12:42:26 +0300

    Basic vue

--- kms-client-vue/src/main.ts
+++ kms-client-vue/src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 createApp(App)
     .use(store)
     .use(router)
